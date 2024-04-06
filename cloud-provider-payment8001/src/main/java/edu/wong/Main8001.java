0b09d17391dafc43c77c71e3542e425d9d34bc7b
chenyunzhao <chenyunzhao@eastmoney.com>
2024-04-06 16:27:00 +0800

    feat: p28 consul配置实时刷新

--- cloud-provider-payment8001/src/main/java/edu/wong/Main8001.java
+++ cloud-provider-payment8001/src/main/java/edu/wong/Main8001.java
@@ -3,8 +3,10 @@ package edu.wong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@RefreshScope
 @EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("edu.wong.mapper")
