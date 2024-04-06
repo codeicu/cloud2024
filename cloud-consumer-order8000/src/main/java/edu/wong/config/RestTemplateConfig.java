9bab03d28e33f2eed6b6c82ac14acc5c928ed72d
chenyunzhao <chenyunzhao@eastmoney.com>
2024-04-06 17:18:55 +0800

    feat: P34 LoadBalancer 负载算法切换

--- cloud-consumer-order8000/src/main/java/edu/wong/config/RestTemplateConfig.java
+++ cloud-consumer-order8000/src/main/java/edu/wong/config/RestTemplateConfig.java
@@ -1,17 +1,33 @@
 package edu.wong.config;
 
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@LoadBalancerClient(value = "cloud-payment-service", configuration = RestTemplateConfig.class)
 public class RestTemplateConfig {
 
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    ReactorLoadBalancer<ServiceInstance> randomLoadBalancer(Environment env,
+                                                            LoadBalancerClientFactory factory) {
+        String name = env.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RandomLoadBalancer(factory.getLazyProvider(name, ServiceInstanceListSupplier.class)
+                , name);
     }
 
 }
