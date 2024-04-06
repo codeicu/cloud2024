15520bb12f834c96b284f629f4226884c6843907
chenyunzhao <chenyunzhao@eastmoney.com>
2024-04-06 15:51:26 +0800

    feat: P25 consul 服务入驻成功

--- cloud-consumer-order8000/src/main/java/edu/wong/Main8000.java
+++ cloud-consumer-order8000/src/main/java/edu/wong/Main8000.java
@@ -2,10 +2,12 @@ package edu.wong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class Main80 {
+public class Main8000 {
     public static void main(String[] args) {
-        SpringApplication.run(Main80.class);
+        SpringApplication.run(Main8000.class);
     }
 }
--- cloud-provider-payment8001/src/main/java/edu/wong/Main8001.java
+++ cloud-provider-payment8001/src/main/java/edu/wong/Main8001.java
@@ -2,8 +2,10 @@ package edu.wong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("edu.wong.mapper")
 public class Main8001 {
